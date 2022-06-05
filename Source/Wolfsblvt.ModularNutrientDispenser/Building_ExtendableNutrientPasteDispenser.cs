413594c3ca7faf0984bb45beaf495fc116a2022f
Wolfsblvt <wolfsblvt@gmx.de>
2022-06-06 00:49:16 +0200

    First display of npd progress

--- Source/Wolfsblvt.ModularNutrientDispenser/Building_ExtendableNutrientPasteDispenser.cs
+++ Source/Wolfsblvt.ModularNutrientDispenser/Building_ExtendableNutrientPasteDispenser.cs
@@ -1,35 +1,80 @@
-﻿using JetBrains.Annotations;
+﻿using System.Text;
+using JetBrains.Annotations;
 using RimWorld;
+using UnityEngine;
 using Verse;
+using Wolfsblvt.ModularNutrientDispenser.Extensions;
 
 namespace Wolfsblvt.ModularNutrientDispenser
 {
     [UsedImplicitly]
     public class Building_ExtendableNutrientPasteDispenser : Building_NutrientPasteDispenser
     {
-        protected int NutritionCount;
-        protected float ProgressInt;
+        /// <summary>[PERSISTENT] The nutrition that was drawn into and is currently being processed</summary>
+        protected float NutritionRaw;
 
+        /// <summary>[PERSISTENT] The value of already processed nutrition that is readily available</summary>
+        protected float ProcessedNutrition;
+
+        protected float NutritionCapacity => 10f; // Hardcoded at the moment. Will come out of the def with a stat later
+        protected float NutritionPerTick => 0.1f; // Hardcoded at the moment. Will come out of the def with a stat later
+
+        protected float DispensableNutritionCost => DispensableDef.GetStatValueAbstract(StatDefOf.Nutrition);
+        protected int DispensableAvailable => (int)Mathf.Floor(ProcessedNutrition / DispensableNutritionCost);
+
+        [CanBeNull]
         public override Thing TryDispenseFood()
         {
             Log.Message("Oooooh, we are trying to dispense something.");
+
+            if (ProcessedNutrition - DispensableNutritionCost < 0)
+            {
+                Log.Message("Can't dispense meal, not enough available.");
+                return null;
+            }
+
+            ProcessedNutrition -= DispensableNutritionCost;
+
             return base.TryDispenseFood();
+        }
+
+        public override bool HasEnoughFeedstockInHoppers()
+        {
+            if (ProcessedNutrition < DispensableNutritionCost)
+                return false;
+
+            return base.HasEnoughFeedstockInHoppers();
         }
 
         public override void TickRare()
         {
             base.TickRare();
 
-            ProgressInt += 0.1f;
-
-            Log.Warning($"Dispenser Tick. Progress: {ProgressInt}");
+            ProcessedNutrition = ProcessedNutrition.AddCapped(NutritionPerTick, NutritionCapacity);
+            Log.Warning($"Dispenser Tick. Progress: {ProcessedNutrition}");
         }
-        
+
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref NutritionCount, "NutritionCount");
-            Scribe_Values.Look(ref ProgressInt, "progress");
+            Scribe_Values.Look(ref NutritionRaw, nameof(NutritionRaw));
+            Scribe_Values.Look(ref ProcessedNutrition, nameof(ProcessedNutrition));
+        }
+
+        public override string GetInspectString()
+        {
+            var sb = new StringBuilder(255);
+            sb.Append(base.GetInspectString());
+
+            if (sb.Length > 0)
+                sb.AppendLine();
+
+            sb.AppendLine($"Contains {ProcessedNutrition.ToStringDecimalIfSmall()} / {NutritionCapacity} nutrition.");
+            sb.AppendLine($"Progress: {(ProcessedNutrition / NutritionCapacity).ToStringPercent()} (___ left)");
+
+            sb.AppendLine($"Available {Find.ActiveLanguageWorker.Pluralize(DispensableDef.label, DispensableAvailable)}: {DispensableAvailable}");
+
+            return sb.ToString().TrimEndNewlines();
         }
     }
 }
---
+++ Source/Wolfsblvt.ModularNutrientDispenser/Extensions/FloatExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Wolfsblvt.ModularNutrientDispenser.Extensions
+{
+    /// <summary>
+    ///     A class containing helpful extensions for <see cref="float" />.
+    /// </summary>
+    public static class FloatExtensions
+    {
+        /// <summary>
+        ///     Adds the given given value to the base. The result will be capped at the maximum defined.
+        ///     <para />
+        ///     If <paramref name="doNotAddPartial" /> is chosen, the value will not be added if it would exceed the maximum defined.
+        /// </summary>
+        /// <param name="start">The base value to start off from</param>
+        /// <param name="add">The value to subtract</param>
+        /// <param name="cap">The maximum it can reach</param>
+        /// <param name="doNotAddPartial">If true the value will only be added if it doesn't exceed the cap</param>
+        /// <returns>The final result value after the addition</returns>
+        [Pure]
+        public static float AddCapped(this float start, float add, float cap, bool doNotAddPartial = false)
+        {
+            // Special check for not adding partial values, only the full 'subtract'
+            if (doNotAddPartial && start + add > cap)
+                return start;
+
+            return Math.Min(start + add, cap);
+        }
+
+        /// <summary>
+        ///     Subtracts the given given value from the base. The result will be capped at the minimum defined.
+        ///     <para />
+        ///     If <paramref name="doNotSubtractPartial" /> is chosen, the value will not be subtracted if it would exceed the minimum defined.
+        /// </summary>
+        /// <param name="start">The base value to start off from</param>
+        /// <param name="subtract">The value to subtract</param>
+        /// <param name="cap">The maximum it can reach</param>
+        /// <param name="doNotSubtractPartial">If true the value will only be subtracted if it doesn't exceed the cap</param>
+        /// <returns>The final result value after the subtraction</returns>
+        [Pure]
+        public static float SubtractCapped(this float start, float subtract, float cap, bool doNotSubtractPartial = false)
+        {
+            // Special check for not adding partial values, only the full 'subtract'
+            if (doNotSubtractPartial && start + subtract > cap)
+                return start;
+
+            return Math.Min(start + subtract, cap);
+        }
+    }
+}
